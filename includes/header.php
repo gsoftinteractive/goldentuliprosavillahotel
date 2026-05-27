2e24b697ca9bb9115920148589cf74ef1a0a6ba0
Gsoft Interactive Systems Ltd <dev@gsoftinteractive.com>
2026-05-27 18:04:06 +0100

    Push mobile hero content below the nav so the title clears the logo

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@ $transparent_nav = $transparent_nav ?? false;
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/style.css?v=5" />
+    <link rel="stylesheet" href="assets/css/style.css?v=6" />
 </head>
 <body class="<?= $transparent_nav ? 'has-transparent-nav' : '' ?>">
 
